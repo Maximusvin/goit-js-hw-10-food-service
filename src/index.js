06f7a43f02ac0267f08ec4d17e010ba2809e2053
Maximusvin <Maximusvin@gmail.com>
2020-09-30 00:41:49 +0300

    r

--- src/index.js
+++ src/index.js
@@ -1,6 +1,1 @@
 import './styles.css';
-
-import './templates/menu.hbs';
-
-import './js/theme-switch';
-import './js/menu';
--- src/js/menu.js
+++
@@ -1,8 +0,0 @@
-import menu from '../menu.json';
-import templateMenu from '../templates/menu.hbs';
-
-const menuRef = document.querySelector('.js-menu');
-
-const html = templateMenu(menu);
-
-menuRef.insertAdjacentHTML('beforeend', html);
